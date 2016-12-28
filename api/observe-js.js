1954f3e97d6f668cb02e24f27ec7734b0f8b332b
Michael Giuffrida <michaelg@michaelg.us>
2016-12-28 12:16:01 -0800

    Object.observe polyfill

---
+++ api/observe-js.js
@@ -0,0 +1,54 @@
+(function() {
+  if (typeof Object.observe != 'undefined')
+    return;
+
+  Object.observe = function(obj, callback, acceptList) {
+    var observer = new ObjectObserver(obj);
+    if (typeof acceptList == 'undefined') {
+      acceptList = ['add', 'update', 'delete', 'reconfigure', 'setPrototype',
+                    'preventExtensions'];
+    }
+
+    observer.open(function(added, removed, changed, getOldValueFn) {
+      var changes = [];
+      if (acceptList.includes('add')) {
+        Object.keys(added).forEach(function(property) {
+          changes.push({
+            name: property,
+            object: obj,
+            type: 'add',
+          });
+        });
+      }
+      if (acceptList.includes('update')) {
+        Object.keys(changed).forEach(function(property) {
+          changes.push({
+            name: property,
+            object: obj,
+            type: 'update',
+            oldValue: getOldValueFn(property),
+          });
+        });
+      }
+      if (acceptList.includes('delete')) {
+        Object.keys(removed).forEach(function(property) {
+          changes.push({
+            name: property,
+            object: obj,
+            type: 'delete',
+            oldValue: getOldValueFn(property),
+          });
+        });
+      }
+      if (!changes.length)
+        return;
+      callback(changes);
+    });
+
+    return obj;
+  };
+
+  setInterval(function() {
+    Platform.performMicrotaskCheckpoint();
+  }, 10);
+})();
